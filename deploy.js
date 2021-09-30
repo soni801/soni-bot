78fe0d81e8aed36765d23cf2d341bca7c20c2b4b
Soni <soni801_@outlook.com>
2021-09-30 22:36:33 +0200

    Load commands from json

--- deploy.js
+++ deploy.js
@@ -1,14 +1,10 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const { clientId, token } = require("./config.json");
 
-const commands = [
-    new SlashCommandBuilder().setName("first").setDescription("first command"),
-    new SlashCommandBuilder().setName("second").setDescription("second command")
-].map(command => command.toJSON());
+const commands = require("./commands.json");
 
 const rest = new REST({ version: "9" }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
-    .then(() => console.log("Successfully registered application commands."));
+    .then(() => console.log("Successfully deployed commands."));
