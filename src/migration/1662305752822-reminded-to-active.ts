98e8cde33ef52a732cc5842e9ce067f9bbc71a39
Soni <soni801_@outlook.com>
2022-09-04 18:46:58 +0200

    fix: fix migration not setting column default

--- src/migration/1662305752822-reminded-to-active.ts
+++ src/migration/1662305752822-reminded-to-active.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 /**
  * Migrates the reminded column of the reminder entity to a superior active model.
@@ -7,13 +7,13 @@ export class remindedToActive1662305752822 implements MigrationInterface
 {
     public async up(queryRunner: QueryRunner): Promise<void>
     {
-        await queryRunner.renameColumn('reminder', 'reminded', 'active');
+        await queryRunner.changeColumn('reminder', 'reminded', new TableColumn({ name: 'active', type: 'boolean', default: true }));
         await queryRunner.query('update reminder set active = not active');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void>
     {
-        await queryRunner.renameColumn('reminder', 'active', 'reminded');
+        await queryRunner.changeColumn('reminder', 'active', new TableColumn({ name: 'reminded', type: 'boolean', default: false }));
         // noinspection SqlResolve - IntelliJ is being dumb
         await queryRunner.query('update reminder set reminded = not reminded');
     }
