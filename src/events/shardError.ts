4c2fa769e7fc8a93381aec2500f4f74fbc483533
Soni <soni801_@outlook.com>
2022-09-06 19:51:51 +0200

    fix: try to fix websocket disconnects

--- src/events/shardError.ts
+++ src/events/shardError.ts
@@ -2,21 +2,24 @@ import { event } from '../types/events';
 import Client from '../util/Client';
 
 /**
- * The invalidated event
+ * The shardError event
  *
  * @param {Client<true>} client The Client to use for the event
+ * @param {Error} error The error that is received
  * @returns {Promise<Client<true>>} The provided Client
  *
  * @author Soni
- * @since 6.0.0
- * @see {@link ClientEvents.invalidated}
+ * @since 6.2.2
+ * @see {@link ClientEvents.shardError}
  */
-const invalidated: event<'invalidated'> = async (client: Client<true>) =>
+const shardError: event<'shardError'> = async (client: Client<true>, error: Error) =>
 {
+    console.error('A websocket connection encountered an error:', error);
+
     client.logger.error('Lost connection to websocket, reconnecting...');
     await client.login().then(() => client.logger.info('Successfully reconnected'));
 
     return client;
 };
 
-export default invalidated;
+export default shardError;
