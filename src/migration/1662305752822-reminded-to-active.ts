1182375f69e25218e950f33372404a44c96ab0dd
Soni <soni@recalstudios.net>
2024-10-06 23:49:24 +0200

    fix: only run migration if needed

--- src/migration/1662305752822-reminded-to-active.ts
+++ src/migration/1662305752822-reminded-to-active.ts
@@ -12,14 +12,19 @@ export class remindedToActive1662305752822 implements MigrationInterface
 {
     public async up(queryRunner: QueryRunner): Promise<void>
     {
-        await queryRunner.changeColumn('reminder', 'reminded', new TableColumn({ name: 'active', type: 'boolean', default: true }));
-        await queryRunner.query('update reminder set active = not active');
+        if (await queryRunner.hasColumn('reminder', 'reminded'))
+        {
+            await queryRunner.changeColumn('reminder', 'reminded', new TableColumn({ name: 'active', type: 'boolean', default: true }));
+            await queryRunner.query('update reminder set active = not active');
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void>
     {
-        await queryRunner.changeColumn('reminder', 'active', new TableColumn({ name: 'reminded', type: 'boolean', default: false }));
-        // noinspection SqlResolve - IntelliJ is being dumb
-        await queryRunner.query('update reminder set reminded = not reminded');
+        if (await queryRunner.hasColumn('reminder', 'active'))
+        {
+            await queryRunner.changeColumn('reminder', 'active', new TableColumn({ name: 'reminded', type: 'boolean', default: false }));
+            await queryRunner.query('update reminder set reminded = not reminded');
+        }
     }
 }
