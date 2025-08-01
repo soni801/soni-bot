a9ef86bfd101e476accb775179b3669c3e62e19f
Soni <soni@recalstudios.net>
2025-08-01 15:14:13 +0200

    feat: reimplement `family` command

---
+++ src/commands/family.ts
@@ -0,0 +1,65 @@
+import {Command} from "../types/Command";
+import Client from "../util/Client";
+import Logger from "../util/Logger";
+import {ChatInputCommandInteraction, Message, SlashCommandBuilder} from "discord.js";
+
+/**
+ * The family command
+ *
+ * @author Soni
+ * @since 7.3.0
+ * @see {@link Command}
+ */
+export default class Family implements Command
+{
+    name = 'family';
+    description = 'Post the family tree';
+    client: Client;
+    logger = new Logger(Family.name);
+    category: 'fun' = 'fun';
+
+    /**
+     * Creates a new family command
+     *
+     * @param {Client} client The Client the command is attached to
+     *
+     * @author Soni
+     * @since 7.3.0
+     * @see {@link Client}
+     */
+    constructor(client: Client)
+    {
+        this.client = client;
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param {ChatInputCommandInteraction<"cached">} i The command interaction
+     * @returns {Promise<Message<boolean>>} The reply sent by the bot
+     *
+     * @author Soni
+     * @since 7.3.0
+     * @see {@link ChatInputCommandInteraction}
+     */
+    async execute(i: ChatInputCommandInteraction<'cached'>): Promise<Message>
+    {
+        return await i.editReply({ embeds: [
+            this.client.defaultEmbed()
+                .setTitle('The family tree:tm:')
+                .setImage('https://files.soni.recalstudios.net/family.png')
+        ] });
+    }
+
+    /**
+     * The slash command builder for this command interaction
+     *
+     * @returns {Promise<SlashCommandBuilder>} The slash command builder for this command interaction
+     */
+    async slashCommand(): Promise<SlashCommandBuilder>
+    {
+        return new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description);
+    }
+}
