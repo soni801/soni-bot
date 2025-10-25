5bfcd2464b36ffaf8e8a0b214b80d950152f7baf
Soni <soni@recalstudios.net>
2025-10-25 11:16:44 +0200

    fix: properly embed `/family` image
    Discord's embed rules are really stupid and I need to format my parameters in a specific way because of course I do

--- src/commands/family.ts
+++ src/commands/family.ts
@@ -45,12 +45,18 @@ export default class Family implements Command
     async execute(i: ChatInputCommandInteraction<'cached'>): Promise<Message>
     {
         // Use current time as parameter for the image to avoid Discord caching
-        const seed = new Date().getTime();
+        const seed = Math.floor(new Date().getTime() / 1000 / 120); // Change value every 2 minutes
 
         return await i.editReply({ embeds: [
             this.client.defaultEmbed()
                 .setTitle('The family tree:tm:')
-                .setImage(`https://files.soni.recalstudios.net/family.png?${seed}`)
+                .addFields([
+                    {
+                        name: '\u200B',
+                        value: '-# Not loading? [Open in browser](https://files.soni.recalstudios.net/family.png)'
+                    }
+                ])
+                .setImage(`https://files.soni.recalstudios.net/family.png?ts=${seed}`)
         ] });
     }
 
